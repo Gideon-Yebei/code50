615345217b4610a1b55ab8fdbe5fc272a8c30376
Gideon-Yebei <0025.2022@students.mnu.ac.ke>
2024-02-04 05:30:33 +0300

    add mario-more

---
+++ Problem_Set_1/mario-more/mario.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt the user for the pyramid's height
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+
+    // Print a pyramid of that height
+    for (int i = 0; i < n; i++)
+    {
+        // Print spaces
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            printf(" ");
+        }
+        // Print '#' characters
+        printf("*");
+
+        // print space between
+        printf(" ");
+
+        // Print '#' characters
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        // Move to the next line after each row
+        printf("\n");
+    }
+    return 0;
+}
